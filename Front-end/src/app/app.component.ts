d2c4df754e0a67788ac86835aab9ae1058acf9e6
SanderJanssen91 <janssen.sander@live.nl>
2017-06-23 14:49:27 +0200

    mock-data update

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+  template: `
+  <body>
+  <h1>{{title}}</h1>
+  <h2>Products</h2>
+  <product-table [products]="products"></product-table>
+  <div>
+    <p>Want to order something? Please contact us on {{phoneNumber}} or {{emailAdress}}</p>
+  </div>
+</body>
+  `,
+/*  templateUrl: './app.component.html',*/
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService],
+  })
+
+export class AppComponent implements OnInit {
+  title = 'Pet-Supplies';
+  phoneNumber = '06-12345678';
+  emailAdress = 'order@pet-supplies.com';
+  products: Product[];
+
+  constructor(private productService: ProductService) {
+  };
+  
+  getProducts(): void {
+    this.productService.getProducts().then(products => this.products = products);
+  };
+
+  ngOnInit(): void {
+    this.getProducts();
+}
 }
